090a57f098b247af6650f547d21e525e87bb20bc
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-03 16:25:27 +0600

    Handling Events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 const Todo = (props) => {
+  const handleDelete = () => {
+    console.log("Delete Button Clicked!");
+    console.log(props.text);
+  };
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={handleDelete}>
+          DELETE
+        </button>
       </div>
     </div>
   );
