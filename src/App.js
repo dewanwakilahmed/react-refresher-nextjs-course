6659c9a02318f0dccd2838130f33e7e713d6d94c
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-03 16:01:18 +0600

    More Component Work & Styling with CSS Classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My Todos</h1>
+
+      <div className="card">
+        <h2>TITLE</h2>
+        <div className="actions">
+          <button className="btn">DELETE</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
