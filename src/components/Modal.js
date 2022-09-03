ceb6dc95799cd785e3d0142185d2ae7635427330
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-09-03 16:32:43 +0600

    Adding More Components

---
+++ src/components/Modal.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn btn--alt">Confirm</button>
+      <button className="btn">Cancel</button>
+    </div>
+  );
+};
+
+export default Modal;
